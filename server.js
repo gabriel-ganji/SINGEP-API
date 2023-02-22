bc64312412f145e4eeec456f3e7ceea9eb4226e9
Gabriel Ganji <gabrielganji01011010@gmail.com>
2023-02-22 09:23:49 -0300

    server running

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.listen(300, () => {
+//body-parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({name: "Json"});
+});
+
+app.listen(3033, () => {
     console.log("Server running on port 3033.");
 });
