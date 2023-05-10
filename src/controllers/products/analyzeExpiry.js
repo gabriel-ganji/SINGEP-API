48ce9f0028e187933f5ade05373d9007b16a7222
Gabriel Ganji <gabrielganji01011010@gmail.com>
2023-05-10 19:25:25 -0300

    update analyzeExpiry

--- src/controllers/products/analyzeExpiry.js
+++ src/controllers/products/analyzeExpiry.js
@@ -2,40 +2,83 @@ const { default: axios } = require("axios");
 const Product = require("../../database/models/Product");
 const Notify = require("../../database/models/Notify");
 
+const sendMessage = async (i) => {
+
+    console.log("sendMessage");
+
+    const message = `Olá. O produto *${i.name}* do lote *${i.lote}* venceu! Você tem *${i.totalun}* unidades vencidas deste produto, certifique-se de tirá-los de circulação.`;
+    const number = `55${i.whatsappOwner}@c.us`;
+
+    await axios.post("http://localhost:3033/send", {number: number, message: message});
+
+}
+
 const AnalyzeDate = async (whatsapp) => {
 
     try {
 
-       return await Product.find({"whatsappOwner": whatsapp}).then(async (data) => {
+       return await Product.find({"whatsappOwner": whatsapp}).then(async (products) => {
 
             let yourDate = new Date();
             const dateNow = yourDate.toISOString().split('T')[0];
+            let account = 0;
+
+            const resultNotify = await Notify.find({"whatsappOwner": whatsapp});
             
-            for(i of data){
+            for (let product of products) {
                 
-                if(String(i.expiry) < dateNow){
+                if (String(product.expiry) < dateNow) {
+                    console.log(product);
+
+                    if (resultNotify[0] == null) {
+                        console.log(resultNotify[account]);
+                        sendMessage(product);
+                        try {
+
+                            let prodName = product.name;
+                            let prodLote = product.lote;
+                            let obj = {whatsappOwner: whatsapp, prodName, prodLote, latestSubmission: dateNow, created_at: new Date(), updated_at: new Date()};
+                            Notify.create(obj);
                     
-                    const message = `Olá. O produto *${i.name}* do lote *${i.lote}* venceu! Você tem *${i.totalun}* unidades vencidas deste produto, certifique-se de tirá-los de circulação.`;
-                    const number = `55${i.whatsappOwner}@c.us`;
-
-                    await axios.post("http://localhost:3033/send", {number: number, message: message});
+                        } catch (error) {
                     
-                    try {
+                            console.log(error);
+                    
+                        }
 
-                        let prodName = i.name;
-                        let prodLote = i.lote;
-                        let obj = {whatsappOwner: whatsapp, prodName, prodLote, latestSubmission: dateNow, created_at: new Date(), updated_at: new Date()};
-                        Notify.create(obj);
+                    } else {
+                        
+                        let notifyLatestSubmission = resultNotify[0].latestSubmission.toISOString().split('T')[0];
+                        console.log("else abaixo de let notify...");
+                        console.log(notifyLatestSubmission, dateNow);
 
-                    } catch(error) {
+                        if(notifyLatestSubmission < dateNow){
 
-                        console.log(error);
+                            sendMessage(product);
+                            
+                            try {
+
+                                let prodName = product.name;
+                                let prodLote = product.lote;
+                                let obj = {whatsappOwner: whatsapp, prodName, prodLote};
+                                await Notify.updateOne(obj, {$set: {latestSubmission: dateNow}});
+                        
+                            } catch(error) {
+                        
+                                console.log(error);
+                        
+                            }
+
+                        }
 
                     }
                     
                 }
+                account++;
             }
+
             return {body: "not null", status: 200};
+
         });
         
     } catch(error) {
